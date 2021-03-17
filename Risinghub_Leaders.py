b71c4abc4c3bbcb4bf35621b29ef23aa6b7240bc
StaaiN <omarashrafzaki@gmail.com>
2021-03-17 18:05:09 +0200

    Update Risinghub_Leaders.py

--- Risinghub_Leaders.py
+++ Risinghub_Leaders.py
@@ -13,7 +13,7 @@ client = commands.Bot(command_prefix="!")
 client.remove_command("help")
 
 # bot token
-token = "TOKEN HERE"
+token = "TOKEN_HERE"
 
 # on ready & presence activity event
 @client.event
@@ -37,14 +37,26 @@ gameicon = "https://cdn.discordapp.com/attachments/710552597886664774/8213459098
 
 usage = '''
 !top elo, !top score, !top level, !top vp,
+!top deaths, !top capture, !top prestige,
 !top time, !top kills, !top assists,
-!top deaths, !top capture, !top killstreak,
-!top deathstreak, !top prestige
+!top killstreak, !top deathstreak, 
 '''
+
+# logout the bot
+@client.command(hidden=False, aliases=['Kill', 'logout', 'Logout'])
+async def kill(ctx):
+    dev_id = ID_HERE # it should be an int, for example: dev_id = 289106753277263872
+    if dev_id == ctx.author.id:
+        await client.logout()
+        await client.close()
+        await ctx.send("the bot has logged out")
+    else:
+        await ctx.send("you don't have permission to this command.")
+
 
 # help
 @client.command(pass_context=True, aliases=['Help'])
-@commands.cooldown(1, 5, commands.BucketType.user) # cooldown: 1 is the number of tries & 5 is the seconds
+@commands.cooldown(1, 5, commands.BucketType.user) # cooldown: 1 is the number of tries & 5 is the seconds.
 async def help(ctx):
     async with ctx.typing():
         embed = discord.Embed(
@@ -58,13 +70,13 @@ async def help(ctx):
 
         embed.add_field(name="Usage", value=usage, inline=False)
 
-        embed.add_field(name="Links", value="[RisingHub](https://risinghub.net/) **|** [RisingHub Leaderboard](https://risinghub.net/leaderboard/score)\n \n Developer: <@289106753277263872>", inline=False)
+        embed.add_field(name="Other", value="**!ping |** [RisingHub](https://risinghub.net/) **|** [RisingHub Leaderboard](https://risinghub.net/leaderboard/score)\n \n Developer: <@289106753277263872>", inline=False)
 
         await ctx.send(embed=embed)
 
 # leaderboards
 @client.command(aliases=['Top', 'TOP'])
-@commands.cooldown(1, 5, commands.BucketType.user) # cooldown: 1 is the number of tries & 5 is the seconds
+@commands.cooldown(1, 5, commands.BucketType.user)
 async def top(ctx,*value):
     async with ctx.typing():
         embed = discord.Embed(
@@ -76,7 +88,19 @@ async def top(ctx,*value):
         await ctx.send(embed=embed)
         return        
 
-    values = {"elo", "score", "level", "vp", "time", "kills", "assists", "deaths", "capture", "killstreak", "deathstreak", "prestige"}
+    values = {
+        "elo", "Elo",
+        "score", "Score",
+        "level", "Level",
+        "vp", "Vp", "VP",
+        "time", "Time",
+        "kills", "Kills",
+        "assists", "Assists",
+        "deaths", "Deaths",
+        "capture", "Capture",
+        "killstreak", "Killstreak",
+        "deathstreak", "Deathstreak",
+        "prestige", "Prestige"}
 
     if value[0] in values:
 
@@ -84,48 +108,56 @@ async def top(ctx,*value):
 
         # get token
         url1 = "https://risinghub.net/"
-        response1 = ses.get(url1)
+        try:
+            response1 = ses.get(url1)
+        except Exception:
+            print("Something went wrong")
         soup1 = BeautifulSoup(response1.text, "html.parser")
         token = soup1.find("input")['value']
 
         # login
         url2 = "https://risinghub.net/login"
-        username, password = "USERNAME HERE", "PASSWORD HERE"
+        username, password = "USERNAME_HERE", "PASSWORD_HERE"
         data = f"_token={token}&username={username}&password={password}&submit="
         headers = {
             "content-type": "application/x-www-form-urlencoded",
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
         }
-        response2 = ses.post(url2, data=data, headers=headers)
+        try:
+            response2 = ses.post(url2, data=data, headers=headers)
+        except Exception:
+            print("Something went wrong")
 
         # leaderboard
-        if value[0] == 'elo':
+        if value[0] == ["elo", "Elo"]:
             url3 = "https://risinghub.net/leaderboard/elo"
-        elif value[0] == 'score':
+        elif value[0] == ['score', 'Score']:
             url3 = "https://risinghub.net/leaderboard/score"
-        elif value[0] == 'level':
+        elif value[0] == ['level', 'Level']:
             url3 = "https://risinghub.net/leaderboard/level"
-        elif value[0] == 'vp':
+        elif value[0] in ["vp","Vp", "VP"]:
             url3 = "https://risinghub.net/leaderboard/vp"
-        elif value[0] == 'time':
+        elif value[0] == ['time', 'Time']:
             url3 = "https://risinghub.net/leaderboard/time"
-        elif value[0] == 'kills':
+        elif value[0] == ['kills', 'Kills']:
             url3 = "https://risinghub.net/leaderboard/kills"
-        elif value[0] == 'assists':
+        elif value[0] == ['assists', 'Assists']:
             url3 = "https://risinghub.net/leaderboard/assists"
-        elif value[0] == 'deaths':
+        elif value[0] == ['deaths', 'Deaths']:
             url3 = "https://risinghub.net/leaderboard/deaths"
-        elif value[0] == 'capture':
+        elif value[0] == ['capture', 'Capture']:
             url3 = "https://risinghub.net/leaderboard/capture"
-        elif value[0] == 'killstreak':
+        elif value[0] == ['killstreak', 'Killstreak']:
             url3 = "https://risinghub.net/leaderboard/kill_streak"
-        elif value[0] == 'deathstreak':
+        elif value[0] == ['deathstreak', 'Deathstreak']:
             url3 = "https://risinghub.net/leaderboard/death_streak"
-        elif value[0] == 'prestige':
+        elif value[0] == ['prestige', 'Prestige']:
             url3 = "https://risinghub.net/leaderboard/prestige"
-        response3 = ses.get(url3)
+        try:
+            response3 = ses.get(url3)
+        except Exception:
+            print("Something went wrong")
         soup2 = BeautifulSoup(response3.text, "html.parser")
-        table = soup2.find("table")
         table_body = soup2.find('tbody')
         rows = table_body.find_all('tr')
         for index, row in enumerate(rows):
@@ -141,5 +173,4 @@ async def top(ctx,*value):
         embed.add_field(name="Error", value="Value is not found, ``!help`` for usage info.", inline=False)
         await ctx.send(embed=embed)  
 
-        
 client.run(token)
